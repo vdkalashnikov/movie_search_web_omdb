b019e8b843909fca77a5653609a58fd0c5faf866
vdkalashnikov <adrfirt@gmail.com>
2024-06-30 18:16:28 +0700

    Initial commit

---
+++ script.js
@@ -0,0 +1,83 @@
+function searchMovie() {
+    $('#movie-list').html('')
+    $.ajax({
+        url: 'http://www.omdbapi.com',
+        type: 'get',
+        dataType: 'json',
+        data: {
+            'apikey': 'df957854',
+            's': $('#search-input').val()
+        },
+        success: function(result) { 
+            if(result.Response == "True"){
+                let movies = result.Search;
+                
+                $.each(movies, function(i, data) {
+                    $('#movie-list').append(`
+                        <div class="col-md-3 col-sm-6">
+                        <div class="card mb-3">
+        <img src="`+ data.Poster +`" class="card-img-top" alt="...">
+        <div class="card-body">
+        <h5 class="card-title">`+ data.Title+`</h5>
+        <h6 class="card-subtitle mb-2 text-body-secondary">`+ data.Year +`</h6>
+        <a href="#" style="text-decoration:none;" class="card-link see-detail" data-bs-toggle="modal" data-bs-target="#exampleModal" data-id="`+ data.imdbID +`" >Lihat Detail</a>
+        </div>
+        </div>
+        </div>
+                    `)
+                })
+    
+                $('#search-input').val('')
+            } else {
+                $('#movie-list').html(`<h3 align="center">`+ result.Error +`</h3>`)
+            }
+        }
+    })
+}
+
+$('#search-button').on('click', function() {
+    searchMovie()
+});
+
+$('#search-input').on('keyup', function(e) {
+    if (e.keyCode == 13) {
+        searchMovie()
+    }
+})
+
+$('#movie-list').on('click', '.see-detail', function() {
+    $.ajax({
+        url: 'http://www.omdbapi.com',
+        type: 'get',
+        dataType: 'json',
+        data: {
+            'apikey': 'df957854',
+            'i' : $(this).data('id')
+        },
+        success : function(movie) {
+            if (movie.Response == "True") {
+
+                $('.modal-body').html(`
+                    <div class="container-fluid">
+                    <div class="row">
+                    <div class="col-md-4">
+                    <img src="`+ movie.Poster +`" class="img-fluid"></div>
+
+                    <div class="col-md-8">
+                    <ul class="list-group">
+                    <li class="list-group-item"><h3>`+ movie.Title +`</h3></li>
+                    <li class="list-group-item">Rilis : `+ movie.Released +`</li>
+                    <li class="list-group-item">Genre :  `+ movie.Genre +`</li>
+                    <li class="list-group-item">Durasi : `+ movie.Runtime +`</li>
+                    <li class="list-group-item">Director : `+ movie.Director +`</li>
+                    <li class="list-group-item">Pemeran : `+ movie.Actors +`</li>
+                    </ul>
+                    </div>
+
+                    </div>
+                    </div>
+                    `)
+            }
+        }
+    })
+})
